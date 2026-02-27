5396c4e2f8931d33ad077ccf4ad901aa25a6f846
littleguysbigbusiness <aiden.parker.lgbbs@gmail.com>
2026-02-27 19:05:02 +1100

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,14 +1,18 @@
 from flask import Flask
 from threading import Thread
+import os
 
 app = Flask('')
 
 @app.route('/')
 def home():
-    return "Bot is online!"
+    return "I am alive!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    # Render provides the port via an environment variable called 'PORT'
+    # We use 10000 as a backup if 'PORT' isn't found
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
